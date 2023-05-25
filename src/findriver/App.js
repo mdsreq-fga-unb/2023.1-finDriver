c17f871753e88deffc83fe549921ea7275969c6e
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-05-25 13:25:34 -0300

    Add supabase configs

--- src/findriver/App.js
+++ src/findriver/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import supabase from './supabase';
 
 export default function App() {
   return (
