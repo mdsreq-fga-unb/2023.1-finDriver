039acd4aee43a4345018bcea63ff118271d6e1ba
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-05-25 13:47:08 -0300

    Fix bug: import supabase

--- src/findriver/App.js
+++ src/findriver/App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import supabase from './supabase';
 
 export default function App() {
   return (
