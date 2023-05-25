df9934f1afecd0b22f83b562a940a12365ede20b
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-05-24 21:00:01 -0300

    Update folder struct

--- src/findriver/__tests__/App.test.js
+++ src/findriver/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import App from './App';
+import App from '../App';
 
 describe('<App />', () => {
   it('has 1 child', () => {
