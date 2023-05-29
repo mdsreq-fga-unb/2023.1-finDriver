4195e12db4eec31bf765f70934e01aa1144535cd
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-05-29 08:59:23 -0300

    test: update User.test

--- src/findriver/__tests__/User.test.js
+++ src/findriver/__tests__/User.test.js
@@ -5,6 +5,11 @@ import UserModel from '../model/User.js'
 describe('Supabase', () => {
     it('Insert sucess', async () => {
         let usuario1 = new UserModel("Felipe", "felipe@gmail.com", "1223455", "cachorro", "sorvete");
-        expect(usuario1.insertUser()).toBeTruthy();
+        let usuarioFunc = jest.spyOn(usuario1, 'insertUser');
+
+        usuario1.insertUser();
+
+        expect(usuarioFunc).toHaveBeenCalled();
+        usuarioFunc.mockRestore()
     });
 });
