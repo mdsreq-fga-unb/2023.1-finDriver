074dae79fa0caae1045804644c95b7fb4694b880
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-05-29 11:30:25 -0300

    Fix: file path

---
+++ src/findriver/.env.js
@@ -0,0 +1,4 @@
+export const environment = {
+    SUPABASE_URL: "https://eoucvdzsxmmjhjwequzd.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvdWN2ZHpzeG1tamhqd2VxdXpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUwMzAyNDgsImV4cCI6MjAwMDYwNjI0OH0.IWyHX5FCetOVvYAhjagF8xm5BjKWKoN55Cu7GLqFTMo"
+}
