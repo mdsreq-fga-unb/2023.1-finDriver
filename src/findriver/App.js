3859adf1d117ee46ab0b8958f7401cb04758d3f9
Maria Alice <marialice3003@gmail.com>
2023-05-30 21:38:09 -0300

    add: Tela inicial de boas vindas

--- src/findriver/App.js
+++ src/findriver/App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Register from './view/pages/Register/index'
 import Login from './view/pages/Login';
+import { Welcome } from './view/pages/Welcome';
 
 
 export default function App() {
   return (
     //<Login/>
-    <Register/>
+    //<Register/>
+    <Welcome/>
   );
 }
