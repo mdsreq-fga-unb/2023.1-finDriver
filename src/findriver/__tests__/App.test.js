c9dcec71498e7d5713499adbbf4fdcb360e4298d
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-05-31 14:59:26 -0300

    Update Tests

--- src/findriver/__tests__/App.test.js
+++ src/findriver/__tests__/App.test.js
@@ -4,10 +4,10 @@ import renderer from 'react-test-renderer';
 import App from '../App';
 
 describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
-  });
+    it('has 2 child', () => {
+      const tree = renderer.create(<App />).toJSON();
+      expect(tree.children.length).toBe(2);
+    });
 });
 // testando a biblioteca jest
 // testando a action pela ultima vez
