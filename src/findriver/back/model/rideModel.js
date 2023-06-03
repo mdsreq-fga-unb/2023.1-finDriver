f524d3ca1a2e8f8b17384ec5799a80048f2c741c
Maria Alice <marialice3003@gmail.com>
2023-06-03 19:25:19 -0300

    feat: Criação do objeto corrida/Ride

---
+++ src/findriver/back/model/rideModel.js
@@ -0,0 +1,10 @@
+let Ride = function(idUser, value, kilometerage,application, description, date){
+    this.idUser = idUser;
+    this.value = value;
+    this.kilometerage = kilometerage;
+    this.application = application;
+    this.description = description;
+    this.date = date;
+}
+
+module.exports = Ride;
