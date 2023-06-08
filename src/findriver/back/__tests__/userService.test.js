f6008f155204a706fcc8b687c4a00b2aab4fb7cc
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-06-07 23:00:41 -0300

    test: Update tests and app file
    Para o teste funcionar, tive que mandar as configurações do express
    para o arquivo server.js e manter o app.listen no app
    também evolui na construção dos testes, apesar de não serem praticos,
    ainda

--- src/findriver/back/__tests__/userService.test.js
+++ src/findriver/back/__tests__/userService.test.js
@@ -1,25 +1,21 @@
 import { createUser, getUserByEmail, updateUserById, deleteUserById } from '../service/userService'
-import { supabase } from '@supabase/supabase-js';
 
-
-jest.mock('@supabase/supabase-js', () => ({
-  from: jest.fn(),
-}));
-  
-/*
 describe('Funções do userService', () => {
-    it('createUser', () => {
+    /*
+    test('createUser', () => {
 
     });
+    */
 
-    it('', () => {
+    test('getUserByEmail', async () => {
+            
+    });
+/*    
+    test('updateUserById', () => {
 
     });
-    it('', () => {
+    test('deleteUserById', () => {
 
     });
-    it('', () => {
-
-    });
-    
-});*/
+*/ 
+});
