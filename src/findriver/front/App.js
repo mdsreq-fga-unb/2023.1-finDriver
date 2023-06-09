5ce86d7a0a6877c63e95f6e37ea155dd466cdd43
Ana Catarna <an4catarina@gmail.com>
2023-06-09 18:24:49 -0300

    feat: Criando a tela de perfil

--- src/findriver/front/App.js
+++ src/findriver/front/App.js
@@ -1,31 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Login from './view/pages/Login';
-import Register from './view/pages/Register/index'
-import Welcome from './view/pages/Welcome';
-import SecurityQuestion from './view/pages/SecurityQuestion';
+import Login from "./view/pages/Login";
+import Register from "./view/pages/Register/index";
+import Welcome from "./view/pages/Welcome";
+import SecurityQuestion from "./view/pages/SecurityQuestion";
+import Profile from "./view/pages/Profile";
 
 const Stack = createStackNavigator();
 
-
 export default function App() {
   return (
-    
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Profile">
         <Stack.Screen name="Home" component={Welcome} />
+        <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="SecurityQuestion" component={SecurityQuestion} />
       </Stack.Navigator>
     </NavigationContainer>
-    //<Welcome/>
-    //<Login/>
-    //<Register/>
-    //<SecurityQuestion/>
-    
   );
 }
