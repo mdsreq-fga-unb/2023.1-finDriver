82f78132c6e439efa0ac6434b46eda5490fe6dea
ViictorHugoo <victorhugorodriguesguimaraes@gmail.com>
2023-06-09 19:17:13 -0300

    add: add expense created (front)

--- src/findriver/front/App.js
+++ src/findriver/front/App.js
@@ -9,6 +9,7 @@ import Welcome from './view/pages/Welcome';
 import SecurityQuestion from './view/pages/SecurityQuestion';
 import Home from './view/pages/Home';
 import EditUser from './view/pages/EditUser';
+import AddExpense from './view/pages/AddExpense';
 
 
 const Stack = createStackNavigator();
@@ -18,13 +19,14 @@ export default function App() {
   return (
 
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Edite seu perfil">
+      <Stack.Navigator initialRouteName="Cadastrar Despeza">
         <Stack.Screen name="Bem-vindo" component={Welcome} />
         <Stack.Screen name="Entrar" component={Login} />
         <Stack.Screen name="Registrar" component={Register} />
         <Stack.Screen name="Perguntas de Segurança" component={SecurityQuestion} />
         <Stack.Screen name="Inicio" component={Home}/>
         <Stack.Screen name="Edite seu perfil" component={EditUser} />
+        <Stack.Screen name="Cadastrar Despeza" component={AddExpense} />
       </Stack.Navigator>
     </NavigationContainer>
   );
