5f67cbb89d196ac08cf81e88b5720ac0dfa6bcea
Maria Alice <marialice3003@gmail.com>
2023-06-11 22:43:20 -0300

    feat: Integração ver usuário/perfil

--- src/findriver/front/view/pages/Profile/index.tsx
+++ src/findriver/front/view/pages/Profile/index.tsx
@@ -1,9 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Image, Text, Pressable, StyleSheet, Alert} from 'react-native';
 
 import styles from './styles';
+import dados from '../../../dados';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const Profile = ({ navigation }) => {
+
+    const [user, setUser] = useState();
+    const [token, setToken] = useState('');
+    
+    const fetchUser = async () => {
+        try{
+            const requestOptions = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': token.toString(),
+                },
+            };
+            fetch(`${dados.Url}/api/user/`, requestOptions)
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data.value[0])
+                    setUser(data.value[0])
+                })
+                .catch((err) => console.log(err));
+        } catch (error){
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        const getToken = async () => {
+            try {
+                const value = await AsyncStorage.getItem('token')
+                if (value !== null) {
+                    setToken(value)
+                }
+            } catch (e) {
+                console.log(e)
+            }
+        } 
+    
+        getToken();
+        fetchUser();
+
+    }, []);
 
     return(
         <View style={styles.container}>
@@ -11,9 +57,9 @@ const Profile = ({ navigation }) => {
             <View>
                 <Text style={styles.title}>Perfil</Text>
                 <Text style={styles.label}>Nome:</Text>
-                <Text style={styles.text}>Ana Catarina Linda Perfeita</Text>
+                <Text style={styles.text}>{user?.name}</Text>
                 <Text style={styles.label}>Email:</Text>
-                <Text style={styles.text}>catarina@soulinda.com</Text>
+                <Text style={styles.text}>{user?.email}</Text>
 
                 <Pressable 
                     style={styles.button}
