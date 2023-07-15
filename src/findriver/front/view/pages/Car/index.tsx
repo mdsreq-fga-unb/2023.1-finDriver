7f8458332018dc9607d14efcff189f6c4ac7de90
Maria Alice <marialice3003@gmail.com>
2023-07-15 01:22:51 -0300

    feat: Finalizando o front do editar carro

--- src/findriver/front/view/pages/Car/index.tsx
+++ src/findriver/front/view/pages/Car/index.tsx
@@ -11,11 +11,21 @@ import { TextInput } from 'react-native-gesture-handler';
 const Car = ({ navigation }) => {
     
     const [refreshing, setRefreshing] = useState(false);
-    const [editing, setEditing] = useState(true);
+    const [editing, setEditing] = useState(false);
+
+    const [model, setModel] = useState("Celta");
+    const [year, setYear] = useState("1999");
+    const [licensePlate, setLicensePlate] = useState("FGA309");
+    const [mileage, setMileage] = useState("300903");
+    const [notes, setNotes] = useState("Carro futuro da marys");
 
     const handleEditPress = () => {
         setEditing(!editing);
     };
+
+    const handleCancelPress = () => {
+        setEditing(!editing);
+    }
 
     const handleSavePress = () => {
        /*Enviar os dados pro banco*/ 
@@ -34,42 +44,48 @@ const Car = ({ navigation }) => {
                     <View style={styles.textArea}>
                         <Text style={styles.label}>Modelo:</Text>
                         {editing ? (
-                            <TextInput style={styles.textInput} value=""></TextInput> 
+                            <TextInput style={styles.textInput} value={model} onChangeText={(value) => setModel(value)}></TextInput> 
                         ) : (
-                            <Text style={styles.text}>Celta</Text>
+                            <Text style={styles.text}>{model}</Text>
                         )}
                         <Text style={styles.label}>Ano:</Text>
                         {editing ? (
-                            <TextInput style={styles.textInput} value=""></TextInput> 
+                            <TextInput style={styles.textInput} value={year} onChangeText={(value) => setYear(value)}></TextInput> 
                         ) : (
-                            <Text style={styles.text}>1999</Text>
+                            <Text style={styles.text}>{year}</Text>
                         )}
                         <Text style={styles.label}>Placa:</Text>
                         {editing ? (
-                            <TextInput style={styles.textInput} value=""></TextInput> 
+                            <TextInput style={styles.textInput} value={licensePlate} onChangeText={(value) => setLicensePlate(value)}></TextInput> 
                         ) : (
-                            <Text style={styles.text}>FGA309</Text>
+                            <Text style={styles.text}>{licensePlate}</Text>
                         )}
 
                         <Text style={styles.label}>Kilometragem:</Text>
                         {editing ? (
-                            <TextInput style={styles.textInput} value=""></TextInput> 
+                            <TextInput  keyboardType="numeric" style={styles.textInput} value={mileage} onChangeText={(value) => setMileage(value)}></TextInput> 
                         ) : (
-                            <Text style={styles.text}>30092003</Text>
+                            <Text style={styles.text}>{mileage}</Text>
                         )}
                         <Text style={styles.label}>Observações:</Text>
                         {editing ? (
-                            <TextInput multiline style={styles.textInput} ></TextInput> 
+                            <TextInput multiline style={styles.textInput} value={notes} onChangeText={(value) => setNotes(value)}></TextInput> 
                         ) : (
-                            <Text style={styles.text}>Carro futuro da marys</Text>                
+                            <Text style={styles.text}>{notes}</Text>                
                         )}
                     </View>
                     
                     <View style={styles.buttonArea}>
                         {editing? (
-                            <Pressable style={styles.button} onPress={handleSavePress}>
-                            <Text style={styles.textButton}>Salvar</Text>
-                        </Pressable> 
+                            <View style={styles.buttonArea}>
+                                <Pressable style={[styles.button, {backgroundColor: '#E6332A'}]} onPress={handleCancelPress}>
+                                    <Text style={[styles.textButton, {color: '#f5f5f7'}]}>Cancelar</Text>
+                                </Pressable> 
+                                <Pressable style={styles.button} onPress={handleSavePress}>
+                                    <Text style={styles.textButton}>Salvar</Text>
+                                </Pressable> 
+                            </View>
+                            
                         ): (
                            <Pressable style={styles.button} onPress={handleEditPress}>
                                 <Text style={styles.textButton}>Editar</Text>
